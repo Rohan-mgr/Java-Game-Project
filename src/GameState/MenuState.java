803e07a85ecedc7de1ed2f3ce52d3970a126d0da
Rohan Magar <rohan.magar.415@gmail.com>
2023-05-05 19:47:40 +0545

    finished tileMap/level gif integration

--- src/GameState/MenuState.java
+++ src/GameState/MenuState.java
@@ -53,6 +53,7 @@ public class MenuState extends GameState {
     private void select(){
         if(currentChoice == 0) {
             //start
+            gsm.setState(GameStateManager.LEVEL1STATE);
         }
         if(currentChoice == 1) {
             //help
