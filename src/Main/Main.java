041ff653618f0a55406b81524f9813f4f97917dc
Rohan Magar <rohan.magar.415@gmail.com>
2023-05-08 17:22:41 +0545

    fixed player health bug

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -3,7 +3,7 @@ package Main;
 import javax.swing.JFrame;
 public class  Main {
     public static void main(String[] args) {
-       JFrame window = new JFrame("Dragon Tale");
+       JFrame window = new JFrame("Dragon");
        window.setContentPane(new GamePanel());
        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
        window.setResizable(false);
